cce2f4748d6298a0018f82ead06006b476864988
Liu Peng <vslene@gmail.com>
2024-08-28 17:26:49 +0800

    format blockly js code use js-beautify

--- packages/blockly-extension/src/rocks/BlocklyEditor.tsx
+++ packages/blockly-extension/src/rocks/BlocklyEditor.tsx
@@ -5,6 +5,31 @@ import { javascriptGenerator } from "blockly/javascript";
 import { toolbox } from "~/toolbox";
 import { definitions } from "~/blocks/_blocks";
 import { renderRockChildren } from "@ruiapp/react-renderer";
+import js_beautify, { JSBeautifyOptions } from "js-beautify";
+
+const jsBeautifyOptions = {
+  indent_size: 2,
+  indent_char: " ",
+  max_preserve_newlines: 0,
+  preserve_newlines: true,
+  keep_array_indentation: true,
+  break_chained_methods: false,
+  indent_scripts: "keep",
+  brace_style: "preserve-inline",
+  space_before_conditional: false,
+  unescape_strings: false,
+  jslint_happy: false,
+  end_with_newline: false,
+  wrap_line_length: 0,
+  indent_inner_html: false,
+  comma_first: false,
+  e4x: false,
+  indent_empty_lines: true,
+} as JSBeautifyOptions;
+
+function jsBeautifyCode(code: string): string {
+  return js_beautify(code, jsBeautifyOptions);
+}
 
 function loadBlocklyEditor(context: RockInstanceContext, container: HTMLElement, commands: MutableRefObject<BlocklyEditorCommands>): Blockly.WorkspaceSvg {
   // Register the blocks and generator with Blockly
@@ -141,7 +166,7 @@ export default {
           Blockly.serialization.workspaces.load(JSON.parse(data), workspace, undefined);
           Blockly.Events.enable();
 
-          previewContainer.current.textContent = commands.current.getCodeContents();
+          previewContainer.current.textContent = jsBeautifyCode(commands.current.getCodeContents());
         },
         clear() {
           workspace.clear();
@@ -169,7 +194,7 @@ export default {
         if (e.isUiEvent || e.type == Blockly.Events.FINISHED_LOADING || workspace.isDragging()) {
           return;
         }
-        previewContainer.current.textContent = commands.current.getCodeContents();
+        previewContainer.current.textContent = jsBeautifyCode(commands.current.getCodeContents());
       });
     }
 
