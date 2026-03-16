9df1a9deaab1318cd23ca6ea105259c11e75716d
Fossil <imzshh@gmail.com>
2026-03-16 15:50:02 +0800

    feat(scope): add slots.children to ScopeMeta

--- packages/react-rocks/src/rocks/scope/ScopeMeta.ts
+++ packages/react-rocks/src/rocks/scope/ScopeMeta.ts
@@ -4,5 +4,12 @@ import { SCOPE_ROCK_TYPE } from "./scope-types";
 export default {
   $type: SCOPE_ROCK_TYPE,
 
+  slots: {
+    children: {
+      allowMultiComponents: true,
+      lazyCreate: true,
+    },
+  },
+
   props: {},
 } as RockMeta;
